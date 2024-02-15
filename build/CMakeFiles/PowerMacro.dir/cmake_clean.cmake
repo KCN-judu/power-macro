e647f7dd13c8eb317510033fa77ba60b9f870ea8
KCN-judu <kcnjudu@gmail.com>
2024-02-15 13:00:26 +0800

    build: add MinGW64 build support using CMake

---
+++ build/CMakeFiles/PowerMacro.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PowerMacro.dir/Src/DelayInteraction.cpp.obj"
+  "CMakeFiles/PowerMacro.dir/Src/DelayInteraction.cpp.obj.d"
+  "CMakeFiles/PowerMacro.dir/Src/FileOperations.cpp.obj"
+  "CMakeFiles/PowerMacro.dir/Src/FileOperations.cpp.obj.d"
+  "CMakeFiles/PowerMacro.dir/Src/Interaction.cpp.obj"
+  "CMakeFiles/PowerMacro.dir/Src/Interaction.cpp.obj.d"
+  "CMakeFiles/PowerMacro.dir/Src/InteractionQueue.cpp.obj"
+  "CMakeFiles/PowerMacro.dir/Src/InteractionQueue.cpp.obj.d"
+  "CMakeFiles/PowerMacro.dir/Src/KeyboardInteraction.cpp.obj"
+  "CMakeFiles/PowerMacro.dir/Src/KeyboardInteraction.cpp.obj.d"
+  "CMakeFiles/PowerMacro.dir/Src/MacroFileProc.cpp.obj"
+  "CMakeFiles/PowerMacro.dir/Src/MacroFileProc.cpp.obj.d"
+  "CMakeFiles/PowerMacro.dir/Src/MouseInteraction.cpp.obj"
+  "CMakeFiles/PowerMacro.dir/Src/MouseInteraction.cpp.obj.d"
+  "CMakeFiles/PowerMacro.dir/main.cpp.obj"
+  "CMakeFiles/PowerMacro.dir/main.cpp.obj.d"
+  "PowerMacro.exe"
+  "PowerMacro.exe.manifest"
+  "PowerMacro.pdb"
+  "libPowerMacro.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PowerMacro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
